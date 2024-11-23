1c6718989309741b0fb01cf3c8e409ece7aed55f
Dng <phamdong9854@gmail.com>
2024-11-23 11:06:40 +0700

    method overloading

--- AimsProject/src/main/java/com/yourcompany/app/Aims/Aims.java
+++ AimsProject/src/main/java/com/yourcompany/app/Aims/Aims.java
@@ -5,6 +5,7 @@ public class Aims {
         // Create a new cart
         Cart anOrder = new Cart();
 
+
         // Create new dvd objects and add them to the cart
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
                 "Animation", 90000, "/1/1084.jpg","abc", "Roger Alers", 87, "/2/3.mp3");
@@ -17,6 +18,9 @@ public class Aims {
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("The Lion King3",
                 "Animation3", 70000, "/1/1086.jpg","abc", "Roger Alers", 87, "/2/3.mp3");
         anOrder.addDigitalVideoDisc(dvd3);
+        DigitalVideoDisc[] dvdList = {dvd1, dvd2};
+        anOrder.addDigitalVideoDisc(dvdList);
+        anOrder.addDigitalVideoDisc(dvd3, dvd2);
 
 
         // Print total cost of the items in the cart
