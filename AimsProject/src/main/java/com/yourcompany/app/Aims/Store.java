aec547e39b0cf3e0e71eb7b3d01bc4552c4c738b
Dng <phamdong9854@gmail.com>
2024-11-23 17:33:03 +0700

    create store

---
+++ AimsProject/src/main/java/com/yourcompany/app/Aims/Store.java
@@ -0,0 +1,21 @@
+package com.yourcompany.app.Aims;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Store {
+    private ArrayList<DigitalVideoDisc>  items;
+
+    public Store() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addDigitalVideoDisc( DigitalVideoDisc... dvdList) {
+        Collections.addAll(items, dvdList);
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        items.remove(disc);
+    }
+
+}
